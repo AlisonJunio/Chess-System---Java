69a1f2d479c71e9e95e06addeecfabd0661a36b4
Alison Lima <alison-junio@hotmail.com>
2019-08-10 11:13:59 -0300

    Possible moves of a piece

--- src/Chess/pieces/King.java
+++ src/Chess/pieces/King.java
@@ -5,13 +5,18 @@ import Chess.Color;
 import boardgame.Board;
 
 public class King extends ChessPiece {
-
 	public King(Board board, Color color) {
 		super(board, color);
 	}
-	
+
 	@Override
 	public String toString() {
 		return "K";
+	}
+
+	@Override
+	public boolean[][] possibleMoves() {
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		return mat;
 	}
 }
--- src/Chess/pieces/Rook.java
+++ src/Chess/pieces/Rook.java
@@ -4,15 +4,18 @@ import Chess.ChessPiece;
 import Chess.Color;
 import boardgame.Board;
 
-public class Rook extends ChessPiece{
-
+public class Rook extends ChessPiece {
 	public Rook(Board board, Color color) {
 		super(board, color);
 	}
-	
 	@Override
 	public String toString() {
 		return "R";
 	}
 
+	@Override
+	public boolean[][] possibleMoves() {
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		return mat;
+	}
 }
